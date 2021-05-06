e03539ff9bf8f691c3f0d816addd682b145abe2d
Hubercorp <hubertyj@gmail.com>
2021-05-06 23:30:44 +0200

    crawl

---
+++ oldtimertrends/items.py
@@ -0,0 +1,20 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class OldtimertrendsItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    auction_brand : scrapy.Field()
+    auction_model : scrapy.Field()
+    auction_model : scrapy.Field()
+    auction_organizor:  scrapy.Field()
+    action_sales_code: scrapy.Field()
+    auction_restauration_code: scrapy.Field()
+    action_price: scrapy.Field()
+    action_location: scrapy.Field()
+    
